16d21d03020d1253cef4c5b9d44d13a6b962014e
Ng Guoyou <fauxface+github@gmail.com>
2015-12-02 00:26:59 +0800

    Add tabs to Radio page

--- app/assets/javascripts/components/radio.es6.jsx
+++ app/assets/javascripts/components/radio.es6.jsx
@@ -1,13 +1,62 @@
-class Radio extends React.Component {
+class Tabs extends React.Component {
   render () {
-    return (
-      <div>
-        <Queue songs={[React.createElement(Song, { artist: 'lmao', title: 'lmbo' })]} />
+    let tab = function (id) {
+      return React.DOM.a({
+        href: "#" + id,
+        className: this.props.selectedItem === id ? "selected" : "",
+        onClick: this.props.onChange.bind(null, id)
+      }, id);
+    }.bind(this);
 
-        <RadioSkipButton httpMethod="post" radioMethod="/radio/skip" label="Skip" />
-
-        <SongSearch />
-      </div>
+    return React.DOM.div(
+      { className: "tabs" },
+      React.DOM.ul(null,
+        React.DOM.li(null, tab("Radio")),
+        React.DOM.li(null, tab("Songs")),
+      )
     );
   }
 }
+
+class Radio extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = { selectedPage: "Radio" }
+  }
+
+  changePage (id) {
+    this.setState({ selectedPage: id });
+  }
+
+  componentWillMount () {
+    this.changePage(window.location.hash.slice(1));
+  }
+
+  render () {
+    let contents;
+
+    switch (this.state.selectedPage) {
+    case "Radio":
+      contents = React.DOM.div(null,
+        React.createElement(Queue, { songs: [React.createElement(Song, { artist: "lmao", title: "lmbo" })] }),
+        React.createElement(RadioSkipButton, { httpMethod: "post", radioMethod: "/radio/skip", label: "Skip" } )
+      );
+      break;
+    case "Songs":
+      contents = React.DOM.span(null,
+        React.createElement(SongSearch)
+      );
+      break;
+    }
+
+    return React.DOM.div(style,
+      React.createElement(Tabs, { selectedPage: this.state.selectedPage, onChange: this.changePage.bind(this) }),
+      contents
+    );
+  }
+}
+
+var style = {
+  display: "flex",
+  flexDirection: "column"
+};
