2577b031029a070669b92bd01ae579497be21a6d
Ng Guoyou <fauxface+github@gmail.com>
2015-12-04 09:20:30 +0800

    Obfuscate uploaded image names

--- app/models/background_image.rb
+++ app/models/background_image.rb
@@ -1,10 +1,14 @@
 class BackgroundImage < ActiveRecord::Base
   belongs_to :song
-  has_attached_file :image,
-  :styles => {
-    :tiny => "125x125",
-    :small => "250x250",
-    :medium => "500x500"
+  has_attached_file :image, {
+    styles: {
+      tiny: "125x125",
+      small: "250x250",
+      medium: "500x500"
+    },
+    url: "/system/:hash.:extension",
+    hash_secret: "lmaolmaolmao"
   }
+
   validates_attachment_content_type :image, :content_type => [/\Aimage/]
 end
