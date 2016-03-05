08bb29cb69380b0308fd31ccec47f87bae4d033a
Ng Guoyou <fauxface+github@gmail.com>
2016-03-06 02:02:33 +0800

    Make tmp dir for restart.txt if it doesn't exist

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -5,6 +5,7 @@ class AdminController < ApplicationController
 
   def restart
     @user = User.find(session[:user_id])
+    Dir.mkdir('tmp') unless File.exists?(Rails.root.join('tmp'))
     restart_txt_path = Rails.root.join('tmp', 'restart.txt')
     logger.info "Restart initiated by #{@user && @user.name} (#{@user && @user.id})."
     logger.info "touch-ing ${restart_txt_path}"
